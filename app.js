d0aaf0b4c7c0e64b790f21ac266b2d0ffcdac052
AmruthjithSunil <amruthjithsunil@gmail.com>
2023-02-13 13:10:04 +0530

    added model and database

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+require("dotenv").config();
+
 const router = require("./routes/router");
+const sequelize = require("./util/database");
 
 const app = express();
 app.use(router);
 
-app.listen(4000);
+sequelize
+  .sync()
+  .then((result) => {
+    console.log("Database synced");
+    app.listen(process.env.PORT);
+  })
+  .catch((err) => console.log(err));
